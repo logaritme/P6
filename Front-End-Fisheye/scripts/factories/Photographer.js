e723d1018377a22a4af53188d4638aac6a756800
logaritme <sirvana.levy@aol.com>
2022-06-28 06:15:39 +0200

    Layout page photographer.html done. Layout modal photographer.html done. About photographer.html -> HTML=OK CSS=OK JS=NotOK

--- Front-End-Fisheye/scripts/factories/Photographer.js
+++ Front-End-Fisheye/scripts/factories/Photographer.js
@@ -1,3 +1,4 @@
+// 
 // Ma factory des photographe pour la page index.html
 // Il y a un seul photographer à chaque fis que je l'instancie -> pas de s
 export class photographerFactory {
@@ -38,135 +39,11 @@ export class photographerFactory {
     <article>
     <h1>${this._name}</h1>
     <p>${this._city + ', ' + this._country}</p>
-    <p>${this._tagline}</p>
+    <p class="text-color">${this._tagline}</p>
     </article>
-    <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+    <button class="contact-button" onclick="displayModal()">Contactez-moi</button>
     <img src="${picture}"></img>
     </section>`;
     photographersHeaderPlace.appendChild(div);
   }
 }
-// get getUserCardDOM() {
-//   const picture = `./assets/fish-eye_photos/Sample\ Photos/Photographers\ ID\ Photos/${this._portrait}`;
-
-//   const article = document.createElement('article');
-//   article.innerHTML = `<article>
-//   <a>
-//   <img>${picture}</img>
-//   <h2>${this._name}</h2>
-//   </a>
-//   <h3>${this._city}</h3>
-//   <h4>${this._tagline}</h4>
-//   <price>${this._price}</price>
-//   </article>`;
-//   document.body.appendChild(article);
-// }
-
-// Implémentaion de fonction supplémentaire dans ma factory pour la photographer-page.html
-// get getPhotographerPageHeaderDOM() {
-//   const picture = `./assets/fish-eye_photos/Sample\ Photos/Photographers\ ID\ Photos/${this._portrait}`;
-//   const section = document.createElement('div');
-//   section.innerHTML = `<section>
-//   <h1>${this._name}</h1>
-//   <p>${this._city + this._tagline}</p>
-//   <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
-//   <img>${picture}</img>
-//   </section>`;
-//   // Ecrire: document.body.appendChild(section); ou ça: return section;
-//   return section;
-// }
-
-// get portrait() {
-// return `./assets/fish-eye_photos/Sample\ Photos/Photographers\ ID\ Photos/${this._portrait}`;
-// const article = document.createElement('article');
-// article.innerHTML = `<article>
-// <a>
-// <img>${picture}</img>
-// <h2>${this._name}</h2>
-// </a>
-// <h3>${this._city}</h3>
-// <h4>${this._tagline}</h4>
-// <price>${ this._price}</price>
-// </article>`;
-
-// return article
-//   }
-// }
-
-//   }
-// }
-// const { name, portrait, id, city, country, tagline, price } = data;
-
-// function getUserCardDOM() {
-// /*
-// const domElement = `<article>
-//   <a class="" href="${maVariable}">………
-// </article>`
-// */
-
-// const picture = `./assets/fish-eye_photos/Sample\ Photos/Photographers\ ID\ Photos/${portrait}`;
-
-//   // I create an article (boxes img&h2 are in the box anchor, anchor is itself in the box article)
-//   const article = document.createElement('article');
-//   // I create an anchor, img, h2, h3, h4, and a price
-//   const anchor = document.createElement('a');
-//   const img = document.createElement('img');
-//   const h2 = document.createElement('h2');
-//   const h3 = document.createElement('h3');
-//   const h4 = document.createElement('h4');
-//   const dailyPrice = document.createElement('price');
-//   // Link (How to set a dynamic link?)
-//   anchor.setAttribute('href', './photographer.html/#' + id);
-//   // Accessibility
-//   img.setAttribute('alt', 'Photo de profil de ' + name);
-//   anchor.setAttribute(
-//     'alt',
-//     'Lien qui redirige vers les photos et vidéos du photographe' + name
-//   );
-//   // I insert the source of the img
-//   img.setAttribute('src', picture);
-//   // I define the article, anchor and h2
-//   anchor.style.cursor = 'pointer';
-//   h2.textContent = name;
-//   h2.style.fontSize = '36px';
-
-//   // Accessibility
-//   article.setAttribute('title', 'Informations sur le photographe: ' + name);
-
-//   //  I define the anchor, img, h3, h4, and dailyPrice
-//   h3.textContent = city + ', ' + country;
-//   h3.style.color = '#901C1C';
-//   h3.style.fontSize = '13px';
-//   h3.style.margin = '0.2rem 0';
-
-//   h4.textContent = tagline;
-//   h4.style.fontSize = '10px';
-//   h4.style.margin = '0.2rem 0';
-
-//   dailyPrice.textContent = price + `€`;
-//   dailyPrice.style.color = '#757575';
-//   dailyPrice.style.fontSize = '9px';
-//   // I set them all as appendChild in this article
-//   anchor.appendChild(img);
-//   anchor.appendChild(h2);
-//   article.appendChild(anchor);
-//   article.appendChild(h3);
-//   article.appendChild(h4);
-//   article.appendChild(dailyPrice);
-
-//   return article;
-// }
-
-// Because of function getPhotographerPageHeaderDOM() return { getPhotographerPageHeaderDOM } added
-// return {
-//   name,
-//   picture,
-//   id,
-//   city,
-//   country,
-//   tagline,
-//   price,
-//   getUserCardDOM,
-//   getPhotographerPageHeaderDOM,
-// };
-// }
--- Front-End-Fisheye/trash-file.js
+++ Front-End-Fisheye/trash-file.js
@@ -1,6 +1,6 @@
 // .gitignore ne veut pas faire son boulot!
 const blabla = 'blabla';
-let tre = 20;
+let tr = 20;
 
 // Faut-il, en l'état actuel de mon code,
 // que je STRINGIFY mes photographers ou mes medias
@@ -9,16 +9,55 @@ let tre = 20;
 
 // Puis-je refacto mes 3 async function ........ await fetch(URL) ?
 
-// Exo fun, le résultat doit être 12078
+// Result must be 12078 (using ONLY ALPHABET characters (abc, ABC) except JS coding characters( () $ _ . , ; {} [] = => ) and operators characters(+-/*=))
 let t = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'];
 let d = ['a', 'a'];
 d = d.length;
 t = t.length;
 let consoleTotal =
-  Math.exp(t) / (d + d + d + d + d + d) -
-  (t * t * d + t * t * d + t * t * d + t * t + t * t + t * t + t * t) -
-  (t + t + t + t) +
-  d +
-  d;
+  Math.exp(t) / t -
+  (t * t * d + d * t * (d * t) + d * d * (t * t)) -
+  d * d * t +
+  d * d;
 consoleTotal = Math.floor(consoleTotal);
 consoleTotal;
+
+// Même exo méthod .charCodeat + concat
+let o = '$';
+let z = [].length;
+o = o.charCodeAt(z);
+r = o / o;
+tr = r + r + r;
+let trSi = o;
+let di = tr * tr + r;
+let mi = Math.pow(di, tr);
+let R = (tr - r) * trSi + tr + tr + mi * (o / tr);
+R;
+
+six = 'six';
+sixInNumber = +six;
+sixInNumber;
+
+function quiNEnEstPasUne(sixInNumber) {
+  if (sixInNumber !== Boolean) {
+    return (ceci = 'cela');
+  }
+}
+quiNEnEstPasUne(sixInNumber);
+console.log(ceci);
+
+var ornik = {
+  get premier() {
+    if (this.journal.length > 0) {
+      return this.journal[this.journal.length - this.journal.length];
+    } else {
+      return this.journal.length;
+    }
+  },
+  journal: [],
+};
+console.log(ornik.premier); // "actu
+
+const animals = [1, 5, 9, 18, 99];
+const nombresA = [9, 99, 1, 18, 5].sort(a - b);
+console.log(animals.slice(animals.length - (animals.length - 1)));
