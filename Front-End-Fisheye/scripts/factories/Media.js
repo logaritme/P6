6276778ee08e8515f10c6d39cbb640137a896f7a
logaritme <sirvana.levy@aol.com>
2022-07-06 17:50:12 +0200

    Increment Decrement likes and total like insert associated are working, but it has to be improved for a better implementation.

--- Front-End-Fisheye/scripts/factories/Media.js
+++ Front-End-Fisheye/scripts/factories/Media.js
@@ -16,29 +16,22 @@ export class mediaFactory {
   // Ma fonction ( sans utiliser get) d'affichage
   // du template-photos pour photographer-page.html
   getPhotosCardDOM() {
-    // If this._photographerId contains a video -> do not analyse this one
-    // If the document(string) has no date -> do not analyse this one
-    const hasDate = !(this._date == undefined);
     let nameOfPhotographer;
     function giveNameStoredInEachId(photographerId) {
-      if (hasDate) {
-        nameOfPhotographer =
-          photographerId === 243
-            ? 'Mimi'
-            : photographerId === 930
-            ? 'Ellie'
-            : photographerId === 82
-            ? 'Tracy'
-            : photographerId === 527
-            ? 'Nabeel'
-            : photographerId === 925
-            ? 'Rhode'
-            : photographerId === 195
-            ? 'Marcel'
-            : 'ERROR PATH';
-      } else {
-        nameOfPhotographer = 'ERROR PATH';
-      }
+      nameOfPhotographer =
+        photographerId === 243
+          ? 'Mimi'
+          : photographerId === 930
+          ? 'Ellie'
+          : photographerId === 82
+          ? 'Tracy'
+          : photographerId === 527
+          ? 'Nabeel'
+          : photographerId === 925
+          ? 'Rhode'
+          : photographerId === 195
+          ? 'Marcel'
+          : 'ERROR PATH';
     }
     const idForMedia = this._photographerId;
     giveNameStoredInEachId(idForMedia);
@@ -47,41 +40,40 @@ export class mediaFactory {
       const videography = `./assets/fish-eye_photos/Sample\ Photos/${nameOfPhotographer}/${videoOfMedia}`;
       const photosPlace = document.querySelector('.photos-displaying');
       const section = document.createElement('section');
-      // titleOfMedia = titleOfMedia;
-      // idOfMedia = idOfMedia;
-      // likesOfMedia = likesOfMedia;
-      // if else use <img src="${videography}"></img> in innerHTML
-      if (imageOfMedia !== null) {
+      if (imageOfMedia !== undefined) {
         section.innerHTML = `
-      <a>
-      <figure class="dimensions-photos-grapher-page">
-      <img src="${photography}"></img>
-      </figure>
-      <figcaption class="position-fig-grapher-page">
-      <h2>${titleOfMedia}
-      </h2>
-      <div>
-      <span class="likes" id="likes-${idOfMedia}">${likesOfMedia}</span>
-      <i class="fas fa-heart"></i>
-      </div>
-      </figcaption>
-      </a>`;
+            <a>
+              <figure class="dimensions-photos-grapher-page">
+                <img src="${photography}"></img>
+              </figure>
+              <figcaption class="position-fig-grapher-page">
+                <h2>${titleOfMedia}
+                </h2>
+                <div id="likesHearts-${idOfMedia}">
+                  <span class="likes" id="likes-${idOfMedia}">${likesOfMedia}</span>
+                  <i class="fas fa-heart" onclick="addLike(${idOfMedia})" ></i>
+                </div>
+              </figcaption>
+            </a>`;
         photosPlace.appendChild(section);
       } else {
         section.innerHTML = `
-      <a>
-      <figure class="dimensions-photos-grapher-page">
-      <img src="${videography}"></img>
-      </figure>
-      <figcaption class="position-fig-grapher-page">
-      <h2>${titleOfMedia}
-      </h2>
-      <div>
-      <span class="likes" id="likes-${idOfMedia}">${likesOfMedia}</span>
-      <i class="fas fa-heart"></i>
-      </div>
-      </figcaption>
-      </a>`;
+            <a>
+              <figure class="dimensions-photos-grapher-page">
+                <video controls>
+                <source src="${videography}"
+                    type="video/mp4">
+                </video>
+              </figure>
+              <figcaption class="position-fig-grapher-page">
+                <h2>${titleOfMedia}
+                </h2>
+                <div id="likesHearts-${idOfMedia}">
+                  <span class="likes" id="likes-${idOfMedia}">${likesOfMedia}</span>
+                  <i class="fas fa-heart" onclick="addLike(${idOfMedia})" ></i>
+              </div>
+              </figcaption>
+            </a>`;
         photosPlace.appendChild(section);
       }
     }
@@ -96,41 +88,9 @@ export class mediaFactory {
     const insertLikesPlace = document.querySelector('footer');
     const divFooter = document.createElement('div');
     divFooter.innerHTML = `<div>
-    <span></span>
+    <span id="totalLikes"></span>
     <i class="fas fa-heart"></i>
     </div>`;
     insertLikesPlace.insertAdjacentElement('afterbegin', divFooter);
   }
 }
-
-/* Using a switch
-        switch(photographerId) {
-            case 243:
-              nameOfPhotographer = 'Mimi'
-              break;
-            case 930:
-              nameOfPhotographer = 'Ellie'
-              break
-            default:
-              nameOfPhotographer = 'ERROR PATH'
-        }
-      */
-
-/* Using a repeating else if
-      if (photographerId === 243) {
-          nameOfPhotographer = 'Mimi';
-        } else if (photographerId === 930) {
-          nameOfPhotographer = 'Ellie';
-        } else if (photographerId === 82) {
-          nameOfPhotographer = 'Tracy';
-        } else if (photographerId === 527) {
-          nameOfPhotographer = 'Nabeel';
-        } else if (photographerId === 925) {
-          nameOfPhotographer = 'Rhode';
-        } else if (photographerId === 195) {
-          nameOfPhotographer = 'Marcel';
-        } else {
-          nameOfPhotographer = 'ERROR PATH';
-          console.log(photographerId);
-        }
-      */
--- Front-End-Fisheye/scripts/pages/photographer-page.js
+++ Front-End-Fisheye/scripts/pages/photographer-page.js
@@ -5,23 +5,32 @@ import { mediaFactory } from '../factories/Media.js';
 
 async function getAllTheJSONDatas() {
   const data = await fetch('./data/photographers.json').then((response) => response.json());
-  const allTheJSONDatas = JSON.parse(JSON.stringify(data));
-  return allTheJSONDatas;
+  return JSON.parse(JSON.stringify(data));
 }
+
+const fullJSONContent = getAllTheJSONDatas();
+// Debug
+// console.info("All JSON's data:", fullJSONContent);
 
 async function getPhotographers() {
   // Stockera les données dans data dès leur arrivée suite au fetch
   const data = await fetch('./data/photographers.json').then((response) => response.json());
-  const photographers = JSON.parse(JSON.stringify(data.photographers));
-  return photographers;
+  return JSON.parse(JSON.stringify(data.photographers));
 }
+
+const photographers = getPhotographers();
+// Debug
+// console.info("All the photographers:", photographers);
 
 async function getMedias() {
   // Stockera les données dans data dès leur arrivée suite au fetch
   const data = await fetch('./data/photographers.json').then((response) => response.json());
-  const medias = JSON.parse(JSON.stringify(data.media));
-  return medias;
+  return JSON.parse(JSON.stringify(data.media));
 }
+
+const medias = getMedias();
+// Debug
+// console.info("All the media(pic+vid):", medias);
 
 // Ne retourne que le photographe correspondant à l'id affiché dans l'url
 async function displayData(photographers, medias) {
@@ -31,59 +40,30 @@ async function displayData(photographers, medias) {
   TemplatePhotographer.getPhotographerPageHeaderDOM();
   TemplatePhotographer.getInsertPriceCardDOM();
 
-  // Cela ne retourne que les medias du photographe ayant l'id affiché dans l'url
-  // Et n'instancie donc que les medias ayant id === id
+  // Retrieves only the medias's photographer who has the id displayed in the url
+  // And instances so only the medias having id === id
   const mediasFiltereds = medias.filter((media) => media.photographerId === id);
   let totalLikes = 0;
 
   mediasFiltereds.forEach((mediasFiltered) => {
     totalLikes += mediasFiltered.likes;
-
-    // Try to increment likes on clicking on the heart ( let's see about decrement later)
-    // 1. Listener or onclick on the heart elmt
-    // 2. Create a function that increment +1 (or ++) and store this in the var newValue (to create)
-    //    ( base value var oldValueLike (to create) is the media.likes or this._likes in the factory)
-    // 3. Inject this var newValueLike using textContent in the span appropriated
-    // 4. Use classList contains/add/remove "liked" class to know what to do when user is clicking on the heart
-    // Try now
-
-    /*console.log(medias.likes);
-    function addLike() {
-      newValueLike = oldValueLike++;
-      }
-      addLike();
-      console.info("newValueLike:", newValueLike);*/
-    // END of Try now
   });
-
   // Display the total numbersOfLikesInsert in the span
   let numbersOfLikesInsert = totalLikes;
-  console.log(numbersOfLikesInsert);
-
   const TemplateLikesInsert = new mediaFactory(photographer);
   TemplateLikesInsert.getInsertLikesCardDOM();
-
+  // Node DOM to set on the total number of likes in the insert
   document.querySelector('footer>div>div>span').textContent = numbersOfLikesInsert;
 
   // This is returning the const = mediasFiltereds;
   mediasFiltereds.forEach((mediasFiltered) => {
-    // Declares variables for addLike function
-    let oldValueLike = mediasFiltered.likes; // 88
-    // Function called by the listener
-    function addLike() {
-      oldValueLike = oldValueLike +1;
-      console.info('inFunctionNewValueLike:', oldValueLike);
-      return oldValueLike;
-    }
-    const iconeHeart = document.querySelectorAll('section>a>figcaption>div>i');
-    iconeHeart.forEach((clickHeart) => clickHeart.addEventListener('click', addLike));
-    console.info('oldValueLike:', oldValueLike); // 88
+    // QUESTION MENTOR: Why I can't inject this let oldValueLike using textContent in the span appropriated ?
+    // Next step-> Have to use classList contains/add/remove "liked" class to know what to do when user is clicking on the heart
     // Plays the mediaFactory on each media filtered
-    const TemplateMedia = new mediaFactory(mediasFiltered);
+    const TemplateMedia = new mediaFactory(mediasFiltered, medias);
     TemplateMedia.getPhotosCardDOM();
-
-    document.querySelectorAll(`#likes-${medias.id}`).textContent = oldValueLike;
-    console.info('oldValueLike:', oldValueLike); // 88
+    // Debug
+    // console.info('NodeListSpan(afterInjection):', spanNumber);
   });
 }
 
@@ -97,3 +77,9 @@ async function init() {
 
 // Starts the series of nested functions
 init();
+
+
+
+// Listener on click on the heart plays function addLike
+// const iconeHeart = document.querySelectorAll('section>a>figcaption>div>i');
+// iconeHeart.forEach((clickHeart) => clickHeart.addEventListener('click', addLike));
