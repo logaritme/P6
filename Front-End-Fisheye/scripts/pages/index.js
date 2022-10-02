65e666b3234dd4a08da1887a3744defd23692a7f
logaritme <sirvana.levy@aol.com>
2022-10-02 07:13:18 +0200

    Focus fixed.

--- Front-End-Fisheye/scripts/pages/index.js
+++ Front-End-Fisheye/scripts/pages/index.js
@@ -1,14 +1,12 @@
-
 // Manages index.html page
 
 import { PhotographersFactory } from '../factories/Photographers.js';
 
-document.querySelector('img[class="logo"]').focus();
-
 async function getPhotographers() {
   // Will store the datas in data just when they are retrieved from the fetch
-  const data = await fetch('../Front-End-Fisheye/data/photographers.json').then((response) => response.json())
-  .catch(err => alert(err, ': Failed to fetch data'));
+  const data = await fetch('../Front-End-Fisheye/data/photographers.json')
+    .then((response) => response.json())
+    .catch((err) => alert(err, ': Failed to fetch data'));
   const photographers = JSON.parse(JSON.stringify(data.photographers));
   return photographers;
 }
@@ -18,11 +16,10 @@ async function displayData(photographers) {
     const TemplatePhotographer = new PhotographersFactory(photographer);
     TemplatePhotographer.setUserCardDOM();
   });
-
 }
 
 async function init() {
-  // Will store the datas in photographers just when 
+  // Will store the datas in photographers just when
   // they are retrieved from the fetch contained in the function getPhotographers
   const photographers = await getPhotographers();
   displayData(photographers);
